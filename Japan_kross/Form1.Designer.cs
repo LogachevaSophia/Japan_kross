f96cfd8c53f252497ec5fda666b1029c91bfd517
София <48216113+LogachevaSophia@users.noreply.github.com>
2022-05-25 09:10:04 +0300

    Add files via upload

---
+++ Japan_kross/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Japan_kross
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Level1 = new System.Windows.Forms.Button();
+            this.Level2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Level1
+            // 
+            this.Level1.Location = new System.Drawing.Point(304, 126);
+            this.Level1.Name = "Level1";
+            this.Level1.Size = new System.Drawing.Size(110, 54);
+            this.Level1.TabIndex = 0;
+            this.Level1.Text = "Easy";
+            this.Level1.UseVisualStyleBackColor = true;
+            this.Level1.Click += new System.EventHandler(this.Level1_Click);
+            // 
+            // Level2
+            // 
+            this.Level2.Location = new System.Drawing.Point(304, 186);
+            this.Level2.Name = "Level2";
+            this.Level2.Size = new System.Drawing.Size(114, 67);
+            this.Level2.TabIndex = 1;
+            this.Level2.Text = "Medium";
+            this.Level2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Level2);
+            this.Controls.Add(this.Level1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Level1;
+        private System.Windows.Forms.Button Level2;
+    }
+}
+
---
+++ Japan_kross/Form2.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Japan_kross
+{
+    public partial class Form2 : Form
+    {
+        public int n, m;
+        List<string[]> arr = new List<string[]>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public static void read(ref List<string[]> arr, string path)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        arr.Add(line.Split(' '));
+                    }
+                }
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+      
+        public Form2 (string level)
+        {
+            
+            switch (level)
+            {
+                case "1":
+                    string path = @"C:\Users\Софийкина\Desktop\ПОЛИТЕХ\дз\Процедурное программирование\2 сем\Japan_kross\levels\1\1.txt";
+                    read(ref arr, path);
+ 
+                    setka();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+           
+        }
+        public void setka()
+        {
+            Panel panel = new Panel();
+            panel.AutoSize = true;
+            Controls.Add(panel);
+
+            MessageBox.Show(arr[0].Length.ToString());
+            for (int i = 0; i <= arr.Count; i++)
+            {
+                for (int j = 0; j <= arr[0].Length; j++)
+                {
+                    if (i == 0 & j==0)
+                    {
+                        Label lab = new Label();
+                        int n = 0;
+                        for (int k = 0; k < arr.Count; k++)
+                        {
+                            if (arr[i][k] == "1")
+                            {
+                                n += 1;
+                            }
+                            else
+                            {
+                                if (n != 0)
+                                {
+                                    lab.Text += n.ToString() + " ";
+                                }
+                            }
+                        }
+                        if (n != 0)
+                        {
+                            lab.Text += n.ToString();
+                        }
+                        panel.Controls.Add(lab);
+                        
+                        
+                    }
+                    else
+                    {
+                        if (j==1 & i > 0)
+                        {
+                            Label lab = new Label();
+                            int n = 0;
+                            for (int k = 0; k < arr.Count; k++)
+                            {
+                                if (arr[k][i] == "1")
+                                {
+                                    n += 1;
+                                }
+                                else
+                                {
+                                    if (n != 0)
+                                    {
+                                        lab.Text += n.ToString() + " ";
+                                    }
+                                }
+                            }
+                            if (n != 0)
+                            {
+                                lab.Text += n.ToString();
+                            }
+                            panel.Controls.Add(lab);
+
+                        }
+                    }
+                            
+                    Button but = new Button();
+                    but.Name = Convert.ToString(i)+ Convert.ToString(j);
+                    but.Location = new Point(j*50+50, i*50+50);
+                    but.Size = new Size(50,50);
+                    panel.Controls.Add(but);
+                }
+            }
+
+            this.Size = new Size(panel.Width,panel.Height+50);
+        }
+
+
+    }
+}
